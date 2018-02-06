c11888b57a135e2f71ae05392e03b7e123c2ffe7
Neeraj <neeraj@skybin.io>
2018-02-06 22:53:09 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { UserComponent }      from './user/user.component';
+import { DashboardComponent }      from './dashboard/dashboard.component';
 const routes: Routes = [
-  { path: 'user', component: UserComponent }
+  { path: '', component: UserComponent },
+  { path: 'user', component: DashboardComponent }
+
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
