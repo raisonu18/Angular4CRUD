55457f00c3ecf7a7994c427f823979ca6c6dd24b
Neeraj <neeraj@skybin.io>
2018-02-15 00:23:12 +0530

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { UserComponent }      from './user/user.component';
-import { DashboardComponent }      from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentComponent } from './student/student.component';
+import { CreateComponent } from './student/create/create.component';
+import { ListComponent } from './student/list/list.component';
 const routes: Routes = [
   { path: '', component: UserComponent },
-  { path: 'user', component: DashboardComponent }
+  {
+    path: 'dashboard', component: DashboardComponent,
+    children: [
+      {
+        path: 'Student', component: ListComponent
+      },
+      {
+        path: 'Student/create', component: CreateComponent
+      },
+    ]
 
+  },
 ];
 @NgModule({
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import { UserComponent } from './user/user.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { StudentComponent } from './student/student.component';
+import { ListComponent } from './student/list/list.component';
+import { CreateComponent } from './student/create/create.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    DashboardComponent
+    DashboardComponent,
+    StudentComponent,
+    ListComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
